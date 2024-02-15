17fe416ef81b8988d4e4abd2c4ffed0e975d1e4e
Luciano Rubino <rbn.lcn@gmail.com>
2024-02-15 15:52:35 +0000

    update

--- website/views.py
+++ website/views.py
@@ -7,21 +7,12 @@ from collections import defaultdict
 
 my_view = Blueprint("my_view", __name__)
 
+
 @my_view.route('/')
 def index():
     weekly_data = WeeklyData.query.all()
-    data_by_day = defaultdict(list)
-    for data in weekly_data:
-        data_by_day[data.day].append(data)
-    current_day = 'Monday'
-
-    return render_template('webapp.html', weekly_data=weekly_data, data_by_day=data_by_day, current_day=current_day)
-
-# @my_view.route('/')
-# def index():
-#     weekly_data = WeeklyData.query.all()
-#     generate_weekly_income_chart(weekly_data)
-#     return render_template('webapp.html', weekly_data=weekly_data)
+    generate_weekly_income_chart(weekly_data)
+    return render_template('webapp.html', weekly_data=weekly_data)
 
 def generate_weekly_income_chart(weekly_data):
     weeks = [data.day for data in weekly_data]
@@ -63,52 +54,8 @@ def submit_weekly_data():
     db.session.add(new_weekly_data)
     db.session.commit()
 
+    weekly_data = WeeklyData.query.all()
 
-# @my_view.route('/')
-# def index():
-#     daily_data = DailyData.query.all()
-#     generate_daily_income_chart(daily_data)
-#     return render_template('webapp.html', daily_data=daily_data)
-
-# def generate_daily_income_chart(daily_data):
-#     days = [data.day for data in daily_data]
-#     total_income = [data.total_income for data in daily_data]
-
-#     plt.bar(days, total_income, color='skyblue')
-#     plt.xlabel('Day')
-#     plt.ylabel('Total Income')
-#     plt.title('Daily Total Income')
-
-#     plt.savefig("website/static/mainplot.png", format='png')
-
-# @my_view.route('/daily_data', methods=['POST'])
-# def submit_daily_data():
-#     day = request.form['day']
-#     total_items = float(request.form['total_items'])
-#     most_popular_item = request.form['most_popular_item']
-#     least_popular_item = request.form['least_popular_item']
-#     total_cost = float(request.form['total_cost'])
-#     max_cost = float(request.form['max_cost'])
-#     min_cost = float(request.form['min_cost'])
-#     average_cost = float(request.form['average_cost'])
-#     least_popular_payment = request.form['least_popular_payment']
-#     most_popular_staff = request.form['most_popular_staff']
-
-#     new_daily_data = DailyData(
-#     day = day,
-#     total_items = total_items,
-#     most_popular_item = most_popular_item,
-#     least_popular_item = least_popular_item,
-#     total_cost = total_cost,
-#     max_cost = max_cost,
-#     min_cost = min_cost,
-#     average_cost = average_cost,
-#     least_popular_payment = least_popular_payment,
-#     most_popular_staff = most_popular_staff
-#     )
-
-#     db.session.add(new_daily_data)
-#     db.session.commit()
-
+    generate_weekly_income_chart(weekly_data)
 
     return redirect(url_for('my_view.index'))
